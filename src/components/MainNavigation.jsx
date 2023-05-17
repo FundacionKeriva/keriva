a8de078b7c8ea07f1393c0262a4071fdc30431bc
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-05-16 19:11:46 -0600

    3 br para que s evea bien el celulares

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -15,6 +15,7 @@ export default function MainNav() {
                 <Navigation></Navigation>
                 <br></br>
                 <br></br>
+                <br></br>
                     <Routes>
                         <Route exact path="/"  element={<LandingPage />} />
                         <Route path="/Catalog" element={<Catalog />} />
