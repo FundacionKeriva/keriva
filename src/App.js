4c3c0dd2001fe32ae0835efdd0bc26e68e2f2776
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-05-16 18:27:20 -0600

    Splash desaparece si le doy clik

--- src/App.js
+++ src/App.js
@@ -7,11 +7,23 @@ function App() {
   const [show, setShow] = useState(true);
 
   useEffect(() => {
+    const handleClick = () => {
+      setShow(false);
+    };
+
+    // Agrega el event listener para detectar clics
+    document.addEventListener("click", handleClick);
+
     // Oculta el componente después de 7 segundos
     const timer = setTimeout(() => {
       setShow(false);
-    }, 20000);//7000
-    return () => clearTimeout(timer);
+    }, 7000);
+
+    // Elimina el event listener y limpia el timeout cuando sea necesario
+    return () => {
+      document.removeEventListener("click", handleClick);
+      clearTimeout(timer);
+    };
   }, []);
 
   return (
