74d1a2020ca786f4563aeefbe42007dcba1873f0
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-06-01 21:42:11 -0600

    Validacion tamaño de servicios

--- src/components/servicesKeriva/Catalog.jsx
+++ src/components/servicesKeriva/Catalog.jsx
@@ -16,42 +16,48 @@ export default function Services() {
     }, []);
 
     return (
-        <div style={{ textAlign: "center", alignItems: 'center' }}>
+        <div style={{ textAlign: "center",justifyContent:"center" }}>
             <br></br>
             <br></br>
-            <h1>Conoce nuestros servicios</h1>
+            <h1 >Conoce nuestros servicios</h1>
             <br></br>
-            <Row xs={1} md={1} lg={2} xl={3} className="g-4 justify-content-center cardC-container" >
-                {
-                    services.map((service, index) => (
-                        <Col key={`cata-${index}`}>
-                            <Card
-                                bg="dark"
-                                text="white"
-                                className="custom-cardC"
-                            >
-                                <Card.Body style={{ backdropFilter: 'blur(20px)', maxHeight: "450px" }}>
-                                    <div className="d-flex flex-column">
-                                        <div className="mb-3">
-                                            <img src={service.imageUrl} alt="Imagen" style={{ width: '100%', maxWidth: "100%", height: "250px", maxHeight: "300px" }} />
-                                        </div>
-                                        <div>
-                                            <OverlayTrigger
-                                                placement="top"
-                                                overlay={<Tooltip>{service.name}</Tooltip>}
-                                            >
-                                                <h4 className="single-line">{service.name}</h4>
-                                            </OverlayTrigger>
-                                            <h6>{service.description}</h6>
-                                            <p >Cuota de recuperación: ${service.price} </p>
-                                        </div>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))
-                }
-            </Row>
+            {
+                services.length == 0 ?
+                    (<h2>Lo sentimos, no hay servicios disponibles por el momento</h2>) :
+                    (
+                        <Row xs={1} md={1} lg={2} xl={3} className="g-4 justify-content-center cardC-container" >
+                            {
+                                services.map((service, index) => (
+                                    <Col key={`cata-${index}`}>
+                                        <Card
+                                            bg="dark"
+                                            text="white"
+                                            className="custom-cardC"
+                                        >
+                                            <Card.Body style={{ backdropFilter: 'blur(20px)', maxHeight: "450px" }}>
+                                                <div className="d-flex flex-column">
+                                                    <div className="mb-3">
+                                                        <img src={service.imageUrl} alt="Imagen" style={{ width: '100%', maxWidth: "100%", height: "250px", maxHeight: "300px" }} />
+                                                    </div>
+                                                    <div>
+                                                        <OverlayTrigger
+                                                            placement="top"
+                                                            overlay={<Tooltip>{service.name}</Tooltip>}
+                                                        >
+                                                            <h4 className="single-line">{service.name}</h4>
+                                                        </OverlayTrigger>
+                                                        <h6>{service.description}</h6>
+                                                        <p >Cuota de recuperación: ${service.price} </p>
+                                                    </div>
+                                                </div>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                ))
+                            }
+                        </Row>
+                    )
+            }
         </div>
     );
 }
