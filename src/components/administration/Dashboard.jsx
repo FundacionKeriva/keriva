91b1b71c764491acf7f9cc7f2438ce139caa46e4
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-06-03 00:29:35 -0600

    Tabla lista

--- src/components/administration/Dashboard.jsx
+++ src/components/administration/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Row, Container, Button } from "react-bootstrap";
+import { Row, Container, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { getServices } from '../../api';
 import "./adminDashboard.css";
 
@@ -22,6 +22,18 @@ export default function Dashboard() {
         borderColor: "transparent",
         boxShadow: "0 3px 4px rgba(1, 1, 1, 1)",
         width: "200px"
+    }
+    const styleWatchImage = {
+        background: "#6c2760",
+        borderColor: "transparent",
+        boxShadow: "0 3px 4px rgba(1, 1, 1, 1)",
+        width: "110px"
+    }
+    const styleEdit = {
+        background: "#6c2760",
+        borderColor: "transparent",
+        boxShadow: "0 3px 4px rgba(1, 1, 1, 1)",
+        width: "110px"
     }
 
     return (
@@ -70,14 +82,22 @@ export default function Dashboard() {
                                     <td>{service.name}</td>
                                     <td>{service.price}</td>
                                     <td>{service.description}</td>
-                                    <td>
-                                        <div className="mb-2">
-                                            <Button variant="success" className="buttonCustom" onClick={() => { }}>Ver imagen</Button>
+                                    <td style={{ display: "flex", justifyContent: "center" }}>
+                                        <div >
+                                            <OverlayTrigger
+                                                placement="top"
+                                                overlay={<Tooltip className="tooltip-custom"><img src={service.imageUrl} alt="Imagen" style={{ width: '100%', height: '100%' }} /></Tooltip>}
+                                            >
+                                                <Button style={styleWatchImage}
+                                                >Ver imagen</Button>
+                                            </OverlayTrigger>
                                         </div>
                                     </td>
                                     <td>
-                                        <div className="mb-2">
-                                            <Button variant="warning" className="buttonCustom" onClick={() => { }}>Editar</Button>
+                                        <div style={{ display: "flex", justifyContent: "center" }}>
+                                            <Button style={styleEdit}
+                                                onClick={() => { }}
+                                            >Editar</Button>
                                         </div>
                                     </td>
                                 </tr>
