3afaa45dfcf7f46de02c425d3f81bee8c1f82cd9
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-06-03 20:07:25 -0600

    Listo dashboard

--- src/components/administration/Dashboard.jsx
+++ src/components/administration/Dashboard.jsx
@@ -54,7 +54,7 @@ export default function Dashboard() {
         }
     };
 
-    function handleNewServiceClick(service) {
+    function handleNewServiceClick() {
         setShowModalAdd(true);
     }
 
