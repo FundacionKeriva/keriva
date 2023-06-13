f3d26d9da8018b16e0ea3a07a3fe55d3a8769b44
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-06-13 16:14:33 -0600

    Correo de los desarrolladores

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -10,6 +10,8 @@ import Footer from "./Footer";
 export default function LandPage() {
   const fullpageRef = useRef(null);
   let fullpageInstance = null;
+  const gmailUrl = "https://mail.google.com/mail/?view=cm&to=luisgutierrez5-1@dgb.email&su=Hola%20Luis%20me,%20intersan%20tus%20servicios...";
+   
 
   useEffect(() => {
     fullpageInstance = new fullpage(fullpageRef.current, {
@@ -33,6 +35,12 @@ export default function LandPage() {
       </div>
       <div className="section section-footer">
         <Footer />
+      </div>
+      <div className="section section-devs">
+        <p style={{ fontWeight: "500" }}>Correo de los desarrolladores: <a
+          href={gmailUrl}
+          target="_blank"
+          rel="noopener noreferrer" style={{ color: "#ee00aa" }}>luisgutierrez5-1@dgb.email</a></p>
       </div>
     </div>
   );
