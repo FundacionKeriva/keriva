e026d1ba2a461273a5b077911f8c8d7614718439
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-06-13 17:15:45 -0600

    Estilos en contact us propios

--- src/components/contact/ContactUs.jsx
+++ src/components/contact/ContactUs.jsx
@@ -12,18 +12,18 @@ export default function Contactus() {
     const mapsUrl = "https://www.google.com/maps/place/Aldama+316+A,+Mart%C3%ADnez+Andrade,+20926+Jes%C3%BAs+Mar%C3%ADa,+Ags./@21.9571983,-102.344925,3a,75y,126.94h,69.4t/data=!3m6!1e1!3m4!1sSN7NTVtiEE1w7wF1OTZVow!2e0!7i16384!8i8192!4m7!3m6!1s0x8429ef54e0ef96b1:0xd0ff447556a0e605!8m2!3d21.9572173!4d-102.3448327!10e5!16s%2Fg%2F11c181rb38?entry=ttu";
     const gmailUrl = "https://mail.google.com/mail/?view=cm&to=kerivaac@gmail.com&su=Hola%20Fundación%20Keriva,%20me%20pueden%20dar%20información%20sobre...";
     return (
-        <Row className="footer-container" >
-            <Col xs={10} md={3} className="colc">
-                <h3 className="title">Conoce más</h3>
-                <p className="text"> <i>"Es una fundación sin fines de lucro que apoya a distintos sectores como lo son adultos mayores, adolescentes y niños"</i></p>
+        <Row className="contact-container" >
+            <Col xs={10} md={3} className="contactcolc">
+                <h3 className="contacttitle">Conoce más</h3>
+                <p className="contacttext"> <i>"Es una fundación sin fines de lucro que apoya a distintos sectores como lo son adultos mayores, adolescentes y niños"</i></p>
                 <p style={{color:"#000"}}>- Alejandro M</p>
             </Col>
-            <Col xs={10} md={4} className="colc ">
-                <h3 className="title">Contáctanos</h3>
-                <ul className="text">
+            <Col xs={10} md={4} className="contactcolc ">
+                <h3 className="contacttitle">Contáctanos</h3>
+                <ul className="contacttext">
                     <li>Teléfono: 449 188 88 98</li>
                     <li>Whatsapp: <a
-                        className="purple-link"
+                        className="contactpurple-link"
                         href={whatsUrl}
                         target="_blank"
                         rel="noopener noreferrer">+52 1 449 188 88 98
@@ -31,7 +31,7 @@ export default function Contactus() {
                     </li>
                     <li>
                         Dirección: <a
-                            className="purple-link"
+                            className="contactpurple-link"
                             href={mapsUrl}
                             target="_blank"
                             rel="noopener noreferrer"
@@ -40,7 +40,7 @@ export default function Contactus() {
                     </li>
                     <li>
                         Email: <a
-                            className="purple-link"
+                            className="contactpurple-link"
                             href={gmailUrl}
                             target="_blank"
                             rel="noopener noreferrer">
@@ -49,17 +49,17 @@ export default function Contactus() {
                     </li>
                 </ul>
             </Col>
-            <Col xs={10} md={3} className="colc">
-                <h3 className="title">Síguenos</h3>
+            <Col xs={10} md={3} className="contactcolc">
+                <h3 className="contacttitle">Síguenos</h3>
                 <div>
                     <a href={facebookUrl} target="_blank" rel="noopener noreferrer">
-                        <Image className="icon-footer" src={facebookImage} alt="Facebook" />
+                        <Image className="contacticon-footer" src={facebookImage} alt="Facebook" />
                     </a>
                     <a href={instagramUrl} target="_blank" rel="noopener noreferrer">
-                        <Image className="icon-footer" src={instagramImage} alt="Facebook" />
+                        <Image className="contacticon-footer" src={instagramImage} alt="Facebook" />
                     </a>
                     <a href={whatsUrl} target="_blank" rel="noopener noreferrer">
-                        <Image className='icon-footer' src={whatsappImage} alt='Whatsapp' />
+                        <Image className='contacticon-footer' src={whatsappImage} alt='Whatsapp' />
                     </a>
                 </div>
             </Col>
